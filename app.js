1a26167599545b43401f344524b223f7c0847b97
Sam Rahimi <samrahimi420@gmail.com>
2017-01-11 19:10:33 -0800

    Beginning of MVP UI implementation

--- app.js
+++ app.js
@@ -19,29 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'www')));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
 app.use('/messages', messages);
 
-
-// catch 404 and forward to error handler
-/*
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  //res.render('error');
-});
-*/
 
 module.exports = app;
