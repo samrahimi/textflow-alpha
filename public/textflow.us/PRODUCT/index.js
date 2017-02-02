907d10ec6bc31d0c55245ca0d2a438547998cec9
Sam Rahimi <samrahimi420@gmail.com>
2017-02-01 18:45:08 -0800

    sfnt demo

---
+++ public/textflow.us/PRODUCT/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use('/', express.static(path.join(__dirname, 'www')));
+//app.use('/', express.static(path.join(__dirname, 'public/textflow.us')));
+
+//app.use('/', index);
+// app.use('/users',users);
+// app.use('/messages', messages);
+// app.use('/slack', slack);
+// app.use('/signup', signup);
+// app.use('/contexts', contexts);
+
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,54 @@ router.get('/', function(req, res, next) {
 router.get('/:user_id/results/', function(req, res, next) {
    //TODO, get user too
    mongo.read_data("Results", {user_id:req.params.user_id}, function(err, results){
+     if (!err && results!= null) {
      var sorted = results.sort((a, b) => b.date - a.date)
      //TODO: paging, etc
      res.send(sorted)
+     }
+     else
+     {
+       res.send([])
+     }
    })
 });
+
+router.get('/signup', function(req, res, next){
+  var user = {
+    first_name: req.query.first_name,
+    last_name: req.query.last_name,
+    email: req.query.email,
+    password: req.query.password
+  }
+  mongo.read_data("Users", {email:user.email}, function(err, result){
+    if (result && result._id) {
+      res.send({error:"User already exists"})
+    }
+    else {
+      mongo.write_data("Users", user, function(err, newUser){
+        if (newUser && newUser._id) {
+          res.send(newUser)
+        } else {
+          res.send({error: err})
+        }
+      })
+    }
+  })
+})
+
+router.get('/login', function(req, res, next){
+  var creds = {
+    email: req.query.email,
+    password: req.query.password
+  }
+  mongo.read_data("Users", {email:creds.email, password:creds.password}, function(err, result){
+    if (result && result._id) {
+      res.send(result)
+    } else {
+      res.send({error: "Login incorrect"})
+    }
+  })
+})
 
 
 router.get('/config/:section', function(req, res, next) {
