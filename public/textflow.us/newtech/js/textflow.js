484bdf02a4ae66d5908ec278fd12ef10b11c3f54
Sam Rahimi <samrahimi420@gmail.com>
2017-02-06 14:59:17 -0800

    Alpha web-only release build

--- public/textflow.us/newtech/js/textflow.js
+++ public/textflow.us/newtech/js/textflow.js
@@ -1,138 +1,141 @@
    /* Core scripts for textflow app. This is a single page app
-      with an AngularJS-llike architecture. Functions have been namespaced 
-      window.fn is the global namespace, then window.fn.dashboard for 
-      page-specific code, etc. */
+         with an AngularJS-llike architecture. Functions have been namespaced 
+         window.fn is the global namespace, then window.fn.dashboard for 
+         page-specific code, etc. */
 
-    window.session={};
-
-    
-    /* Global Functions */
-    window.fn = {};
-    window.fn.navigate = function(page, parameters) {
-      $("#kerouac")[0].pushPage(page, {data:parameters})
-    };
-
-    window.fn.isLoggedIn = function() {
-      if (window.user && window.user != null) {
-        return true
-      }
-
-      var userinfo = localStorage.getItem("textflow_user")
-      if (userinfo && userinfo != null) {
-        window.user = JSON.parse(userinfo)
-        return true;
-      } 
-      return false;
-    }
-
-    window.fn.logout= function() {
-      localStorage.removeItem("textflow_user")
-      window.user = null
-      fn.navigate('login.html')
-    };
+   window.session = {};
 
 
-    /* Login page */
-    window.fn.login = {}
-    window.fn.login.initLogin = function() {
-            $("#signup").on("click", function() {
-                fn.navigate("signup.html")
-            })
-            $(".login-button").on("click", function() {
-                var u = $("#email").val()
-                var p = $("#password").val()
-                $.get("/users/login?email="+u+"&password="+p, function(result){
-                    if (result._id) {
-                        localStorage.setItem("textflow_user", JSON.stringify(result))
-                         fn.navigate("dashboard.html")
-                    } else {
-                        alert("Incorrect username or password")
-                    }
-                })
-            })
-    }
+   /* Global Functions */
+   window.fn = {};
+   window.fn.navigate = function(page, parameters) {
+       $("#kerouac")[0].pushPage(page, { data: parameters })
+   };
 
-    /* Signup page */
-    window.fn.signup = {}
-    window.fn.signup.initSignup = function() {
-                $("#signup-button").on("click", function() {
-                var u = $("#signup #email").val()
-                var p = $("#signup #password").val()
-                var f = $("#signup #firstname").val()
-                var l = $("#signup #lastname").val()
+   window.fn.isLoggedIn = function() {
+       if (window.user && window.user != null) {
+           return true
+       }
 
-                $.get("/users/signup?email="+u+"&password="+p+"&first_name="+f+"&last_name="+l, function(result){
-                    if (result._id) {
-                        localStorage.setItem("textflow_user", JSON.stringify(result))
-                        fn.navigate("dashboard.html")
-                    } else {
-                        alert("Something went wrong. Please try again")
-                    }
-                })
-            })
-    }
+       var userinfo = localStorage.getItem("textflow_user")
+       if (userinfo && userinfo != null) {
+           window.user = JSON.parse(userinfo)
+           return true;
+       }
+       return false;
+   }
 
-    window.fn.dynamics = 
-    {}
-        window.fn.dynamics.init = function() {
-          
+   window.fn.logout = function() {
+       localStorage.removeItem("textflow_user")
+       window.user = null
+       fn.navigate('login.html')
+   };
 
-        // google.charts.load('current', {'packages':['gauge']});
-        // google.charts.setOnLoadCallback(drawChart);
-        
-        // function drawChart() {
 
-        //     var data = google.visualization.arrayToDataTable([
-        //         ['Label', 'Value'],
-        //         ['FlowScore', results.overall_score]
-        //     ]);
+   /* Login page */
+   window.fn.login = {}
+   window.fn.login.initLogin = function() {
+       $("#signup").on("click", function() {
+           fn.navigate("signup.html")
+       })
+       $(".login-button").on("click", function() {
+           var u = $("#email").val()
+           var p = $("#password").val()
+           $.get("/users/login?email=" + u + "&password=" + p, function(result) {
+               if (result._id) {
+                   localStorage.setItem("textflow_user", JSON.stringify(result))
+                   fn.navigate("dashboard.html")
+               } else {
+                   alert("Incorrect username or password")
+               }
+           })
+       })
+   }
 
-        //     var options = {
-        //     width: screen.width, height: screen.width,
-        //     redFrom: 0, redTo: 40,
-        //     yellowFrom:40, yellowTo: 60,
-        //     greenFrom:60, greenTo:100,
-        //     minorTicks: 5
-        //     };
+   /* Signup page */
+   window.fn.signup = {}
+   window.fn.signup.initSignup = function() {
+       $("#signup-button").on("click", function() {
+           var u = $("#signup #email").val()
+           var p = $("#signup #password").val()
+           var f = $("#signup #firstname").val()
+           var l = $("#signup #lastname").val()
 
-        //     var chart = new google.visualization.Gauge(document.getElementById('overall_score_chart'));
+           $.get("/users/signup?email=" + u + "&password=" + p + "&first_name=" + f + "&last_name=" + l, function(result) {
+               if (result._id) {
+                   localStorage.setItem("textflow_user", JSON.stringify(result))
+                   fn.navigate("dashboard.html")
+               } else {
+                   alert("Something went wrong. Please try again")
+               }
+           })
+       })
+   }
 
-        //     chart.draw(data, options);
+   window.fn.dynamics = {}
+   window.fn.dynamics.init = function() {
 
-    }
-    /* Compose page */
-    window.fn.compose = {}
 
-    window.fn.compose.analyze = function() {
-      var t = $("#message_text").val()
-      var u = window.user._id
-      $.post("/messages", {text: t, user_id: u}, function(results){
-        window.session.latest = results      
+           // google.charts.load('current', {'packages':['gauge']});
+           // google.charts.setOnLoadCallback(drawChart);
 
-        google.charts.load('current', {'packages':['gauge']});
-        google.charts.setOnLoadCallback(drawChart);
-        
-        function drawChart() {
+           // function drawChart() {
 
-            var data = google.visualization.arrayToDataTable([
-                ['Label', 'Value'],
-                ['FlowScore', results.overall_score]
-            ]);
+           //     var data = google.visualization.arrayToDataTable([
+           //         ['Label', 'Value'],
+           //         ['FlowScore', results.overall_score]
+           //     ]);
 
-            var options = {
-            width: screen.width, height: screen.width,
-            redFrom: 0, redTo: 40,
-            yellowFrom:40, yellowTo: 60,
-            greenFrom:60, greenTo:100,
-            minorTicks: 5
-            };
+           //     var options = {
+           //     width: screen.width, height: screen.width,
+           //     redFrom: 0, redTo: 40,
+           //     yellowFrom:40, yellowTo: 60,
+           //     greenFrom:60, greenTo:100,
+           //     minorTicks: 5
+           //     };
 
-            var chart = new google.visualization.Gauge(document.getElementById('overall_score_chart'));
+           //     var chart = new google.visualization.Gauge(document.getElementById('overall_score_chart'));
 
-            chart.draw(data, options);
-        }
+           //     chart.draw(data, options);
 
-        /*
+       }
+       /* Compose page */
+   window.fn.compose = {}
+
+   window.fn.compose.analyze = function() {
+       var t = $("#message_text").val()
+       var u = window.user._id
+       $.post("/messages", { text: t, user_id: u }, function(results) {
+           window.session.latest = results
+
+           google.charts.load('current', { 'packages': ['gauge'] });
+           google.charts.setOnLoadCallback(drawChart);
+
+           function drawChart() {
+
+               var data = google.visualization.arrayToDataTable([
+                   ['Label', 'Value'],
+                   ['FlowScore', results.overall_score]
+               ]);
+
+               var options = {
+                   width: screen.width,
+                   height: screen.width,
+                   redFrom: 0,
+                   redTo: 40,
+                   yellowFrom: 40,
+                   yellowTo: 60,
+                   greenFrom: 60,
+                   greenTo: 100,
+                   minorTicks: 5
+               };
+
+               var chart = new google.visualization.Gauge(document.getElementById('overall_score_chart'));
+
+               chart.draw(data, options);
+           }
+
+           /*
         window.scores = results.user_scores
         window.raw_scores = results.raw_scores
         var emo, persona = ''
@@ -149,65 +152,66 @@
          $("emotions-list").html(emo)
          $("personality-list").html(persona)
          $("#results_summary").show() */
-      }) 
-    };
+       })
+   };
 
-    window.fn.dashboard = {}
-    window.fn.dashboard.initDashboard = function(user) {
-      $.get("/users/"+window.user._id+"/results/?format=dataset&group=0&chart=0", function(results) {
-                    if (results.length > 0) {
-                        for (var i = 1; i<=5; i++) {
-                          var data = results.map(x => x[i])
-                          $(".trend[data-index="+i+"]").sparkline(data)
-                        }
-                    }
+   window.fn.dashboard = {}
+   window.fn.dashboard.initDashboard = function(user) {
+       $(".profile-name").html(window.user.first_name)
+       $.get("/users/" + window.user._id + "/results/?format=dataset&group=0&chart=0", function(results) {
+           if (results.length > 0) {
+               for (var i = 1; i <= 5; i++) {
+                   var data = results.map(x => x[i])
+                   $(".trend[data-index=" + i + "]").sparkline(data)
+               }
+           }
 
-                        window.session.user_history = results
-                        // google.charts.load('current', {packages: ['corechart', 'line']});
-                        // google.charts.setOnLoadCallback(drawChart);
-      })
+           window.session.user_history = results
+               // google.charts.load('current', {packages: ['corechart', 'line']});
+               // google.charts.setOnLoadCallback(drawChart);
+       })
 
-      /*
-      function drawChart() {
-                var data = new google.visualization.DataTable();
-                data.addColumn('string', 'Day');
-                data.addColumn('number', 'Anger');
-                data.addColumn('number', 'Disgust');
-                data.addColumn('number', 'Fear');
-                data.addColumn('number', 'Joy');
-                data.addColumn('number', 'Sadness');
-                //These are actualyl my emotions. weird.
-                data.addRows(window.session.user_history);
+       /*
+       function drawChart() {
+                 var data = new google.visualization.DataTable();
+                 data.addColumn('string', 'Day');
+                 data.addColumn('number', 'Anger');
+                 data.addColumn('number', 'Disgust');
+                 data.addColumn('number', 'Fear');
+                 data.addColumn('number', 'Joy');
+                 data.addColumn('number', 'Sadness');
+                 //These are actualyl my emotions. weird.
+                 data.addRows(window.session.user_history);
 
-                  var options = {
-                    curveType: 'function',
-                    legend: { position: 'none' }
-                  }
-                var chart = new google.visualization.LineChart(document.getElementById('emotion_chart'));
-                chart.draw(data, options);
+                   var options = {
+                     curveType: 'function',
+                     legend: { position: 'none' }
+                   }
+                 var chart = new google.visualization.LineChart(document.getElementById('emotion_chart'));
+                 chart.draw(data, options);
 
-          }*/
-    } 
+           }*/
+   }
 
-    ons.ready(function() {
-      console.log("Onsen UI is ready!");
-      console.log("Loading User");
-      
-      document.addEventListener('init', function(event){
-        var page = event.target
-        switch(page.id) {
-           case 'dashboard':
-            if (fn.isLoggedIn()) {
-              fn.dashboard.initDashboard();
-            } else
-              fn.navigate('login.html')
-            break
-           case 'login-page':
-            fn.login.initLogin()
-            break;
-          case 'signup':
-            fn.signup.initSignup()
-            break;
-        }
-      })
-    });
+   ons.ready(function() {
+       console.log("Onsen UI is ready!");
+       console.log("Loading User");
+
+       document.addEventListener('init', function(event) {
+           var page = event.target
+           switch (page.id) {
+               case 'dashboard':
+                   if (fn.isLoggedIn()) {
+                       fn.dashboard.initDashboard();
+                   } else
+                       fn.navigate('login.html')
+                   break
+               case 'login-page':
+                   fn.login.initLogin()
+                   break;
+               case 'signup':
+                   fn.signup.initSignup()
+                   break;
+           }
+       })
+   });
